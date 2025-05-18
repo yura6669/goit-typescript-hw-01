fd121a5b4842be4325cbf6b3883b28ab0215b73b
yura6669 <yurii.shyian.01@gmail.com>
2025-05-18 20:45:23 +0300

    Fix generic task 3

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,7 +1,7 @@
 console.log('------------------- Generic Task 3 -------------------');
 
-function merge<T, U>(objA: T, objB: U): T & U { 
-    return Object.assign({}, objA, objB);
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
+  return Object.assign({}, objA, objB);
 }
 
 const objA = { name: 'Yurii' };
